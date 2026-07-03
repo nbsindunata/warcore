c017bb60ce4635c80de51ac9fb81be7fff23471f
Nyoman Sindunata <nyoman.sindunata@yahoo.com>
2026-07-03 11:18:41 +0700

    Create navigation configuration

---
+++ lib/navigation.ts
@@ -0,0 +1,59 @@
+import {
+  LayoutDashboard,
+  Factory,
+  TrendingUp,
+  Swords,
+  Globe,
+  Users,
+  Shield,
+  BookOpen,
+  Settings,
+} from "lucide-react";
+
+export const navigation = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Companies",
+    href: "/companies",
+    icon: Factory,
+  },
+  {
+    title: "Market",
+    href: "/market",
+    icon: TrendingUp,
+  },
+  {
+    title: "Battles",
+    href: "/battles",
+    icon: Swords,
+  },
+  {
+    title: "Countries",
+    href: "/countries",
+    icon: Globe,
+  },
+  {
+    title: "Players",
+    href: "/players",
+    icon: Users,
+  },
+  {
+    title: "Alliance",
+    href: "/alliance",
+    icon: Shield,
+  },
+  {
+    title: "Wiki",
+    href: "/wiki",
+    icon: BookOpen,
+  },
+  {
+    title: "Settings",
+    href: "/settings",
+    icon: Settings,
+  },
+];
